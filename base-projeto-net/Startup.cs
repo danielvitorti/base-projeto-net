bd21e678b34e1a026bbbe92b983172f5d8cbd62a
Daniel Vitor Correa Mendes <daniel.mendes@unimedvr.com.br>
2018-08-28 16:14:25 -0300

    Commit inicial

---
+++ base-projeto-net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(base_projeto_net.Startup))]
+namespace base_projeto_net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
