e8a59fbb23773816008fb91978069718d7e67dec
Daniel Mendes <daniel.vitor.ti@gmail.com>
2018-08-29 00:02:42 -0300

    Adição de arquivos para criação do projeto base baseado no admin lte

--- base-projeto-net/App_Start/BundleConfig.cs
+++ base-projeto-net/App_Start/BundleConfig.cs
@@ -7,25 +7,32 @@ namespace base_projeto_net
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
-        {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+        {   
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                      "~/Scripts/jquery.min.js",
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/fastclick.js",
+                      "~/Scripts/adminlte.min.js",
+                      "~/Scripts/jquery.sparkline.min.js",
+                      "~/Scripts/jquery-jvectormap-1.2.2.min.js",
+                      "~/Scripts/jquery-jvectormap-world-mill-en.js",
+                      "~/Scripts/jquery.slimscroll.min.js",
+                      "~/Scripts/Chart.js",
+                      "~/Scripts/dashboard2.js",
+                      "~/Scripts/demo.js"
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css", //ok
+                      "~/Content/font-awesome.min.css", //ok
+                      "~/Content/ionicons.min.css", //ok
+                      "~/Content/jquery-jvectormap.min.css", //ok
+                      "~/Content/AdminLTE.min.css",
+                      "~/Content/_all-skins.min.css.min.css" //ok
+
+                      ));
         }
     }
 }
