8c451a4777863ea3a46dd84fba88fa2af2a9ed5e
Daniel Vitor Correa Mendes <daniel.mendes@unimedvr.com.br>
2018-09-19 13:14:53 -0300

    Add migration para asp.net identity. #1

---
+++ base-projeto-net/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace base_projeto_net.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<base_projeto_net.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(base_projeto_net.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
